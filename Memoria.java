acc1b84965a85a14c943aa6c8aa7cd57b1ef9ea8
Alan <alan@Jrmungandr.(none)>
2012-03-13 19:27:33 -0600

    Correccion memoria

--- Memoria.java
+++ Memoria.java
@@ -8,8 +8,8 @@ public class Memoria{
     public Memoria(int[] sizes){
         global = new Submemoria(sizes[0], sizes[1], sizes[2], sizes[3], sizes[4]);
         local = new Submemoria(sizes[5], sizes[6], sizes[7], sizes[8], sizes[9]);
-        temporal = new Submemoria(200, 200, 200, 200, 200);
-        constantes = new Submemoria(200, 200, 200, 200, 200);
+        temporal = new Submemoria(sizes[10], sizes[11], sizes[12], sizes[13], sizes[14]);
+        constantes = new Submemoria(sizes[15], sizes[16], sizes[17], sizes[18], sizes[19]);
     }
     
     public Submemoria getGlobal(){return global;}
